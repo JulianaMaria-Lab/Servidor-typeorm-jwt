aac07228f5457b646a97f2f8ea1384153f232f01
JulianaMaria-Lab <juliana.ramos01@fatec.sp.gov.br>
2022-12-11 23:04:53 -0300

    att department controller

--- servidor/src/controllers/DepartmentController.ts
+++ servidor/src/controllers/DepartmentController.ts
@@ -1,6 +1,7 @@
 import AppDataSource from "../data-source";
 import { Request, Response } from 'express';
 import { Department } from '../entities';
+import { department } from ".";
 
 class DepartmentController {
 
@@ -11,14 +12,14 @@ class DepartmentController {
         }
         const object = new Department();
         object.name = name.trim();;
-        const turno: any = await AppDataSource.manager.save(Department, object).catch((e) => {
+        const department: any = await AppDataSource.manager.save(Department, object).catch((e) => {
             // testa se o name é repetido
             if (/(name)[\s\S]+(already exists)/.test(e.detail)) {
                 return { error: 'Já existe um departamento com este nome' };
             }
             return e.message;
         })
-        return res.json(turno);
+        return res.json(department);
     }
 
     public async list(req: Request, res: Response): Promise<Response> {
@@ -30,6 +31,52 @@ class DepartmentController {
         });
         return res.json(object);
     }
+
+    public async update(req: Request, res: Response): Promise<Response> {
+        let { iddepartment, name } = req.body
+        if (!iddepartment) {
+            return res.json({ error: "Identificador inválido" })
+        }
+        const departament: any = await AppDataSource.manager.findOneBy(Department, { iddepartment}).catch((e) => {
+            return { error: e.message }
+        })
+            
+        if (departament && departament.iddepartment) {
+            departament.name = name
+            const r = await AppDataSource.manager.save(Department, departament).catch((e) => {
+                return { error: e.message }
+            })
+                return res.json(r)
+            }
+            else if (department && departament.error) {
+                return res.json(departament)
+            }
+            else {
+                return res.json({ error: "Departamento não localizado" })
+            }
+    }
+
+    public async delete(req: Request, res: Response): Promise<Response> {
+        const { iddepartment } = req.body
+        if (!iddepartment) {
+            return res.json({ error: "Identificador inválido" })
+        }
+        const departament: any = await AppDataSource.manager.findOneBy(Department, { iddepartment}).catch((e) => {
+            return { error: e.message }
+        })
+
+        if (departament && departament.iddepartment) {
+            const r = await AppDataSource.manager.remove(Department, departament).catch((e) => e.message)
+            return res.json(r)
+        }
+        else if (department && departament.error) {
+            return res.json(departament)
+        }
+        else {
+            return res.json({ error: "Departamento não localizado" })
+        }
+    }
+
 
 }
 
--- servidor/src/routes/department.ts
+++ servidor/src/routes/department.ts
@@ -1,17 +1,14 @@
 import { Router } from "express";
 import {department} from "../controllers";
-import { authAdmin } from "../middlewares";
+import { authAdmin, authorization } from "../middlewares";
 const routes = Router();
 
-routes.post('/:departament', authAdmin,department.create);
-//routes.put('/:departament', authAdmin,department.update);
-//routes.delete('/:departament', authAdmin,department.delete);
-//routes.get('/:departament/:user',authorization ,department.get);
+routes.post('/:departament', authAdmin,department.create); //vii. POST: /department somente usuários com perfil admin;
+routes.put('/:departament', authAdmin,department.update); //vii. PUT: /department somente usuários com perfil admin;
+routes.delete('/:departament', authAdmin,department.delete); //vii. DELETE: /department somente usuários com perfil admin;
+routes.get('/:departament/:user', authorization ,department.list); //vii. GET: /department/:user somente usuários logados. 
 //>
 //user poder ser true ou false - true retorna dados dos empregados do departamento
-
-
-//routes.post('/', authAdmin, department.create);
 //routes.get('/:employees', department.list);
 
 export default routes;
